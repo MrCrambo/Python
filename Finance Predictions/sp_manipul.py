66232d0291d23930ab8d3e5ef2929f0455104763
MrCrambo <kadirov.il.crambo@gmail.com>
2020-01-09 21:30:50 +0300

    sp500 loader

--- Finance Predictions/sp_manipul.py
+++ Finance Predictions/sp_manipul.py
@@ -1,5 +1,9 @@
 import bs4 as bs
 import requests
+import os
+import pandas as pd
+import pandas_datareader.data as web
+import datetime as dt
 
 
 def get_sp_tickers():
@@ -13,5 +17,22 @@ def get_sp_tickers():
 
     return tickers
 
+def get_yahoo_data():
+    tickers = get_sp_tickers()
+    if not os.path.exists('sp500_data'):
+        os.makedirs('sp500_data')
+
+    start = dt.datetime(2000, 1, 1)
+    end = dt.datetime(2019, 12, 31)
+
+    for ticker in tickers:
+        if not os.path.exists('sp500_data/{}.csv'.format(ticker)):
+            print('Loading data for {}...'.format(ticker))
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('sp500_data/{}.csv'.format(ticker))
+        else:
+            print('Already loaded {}'.format(ticker))
+
+get_yahoo_data()
 
 
