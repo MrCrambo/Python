95188c81c2d58cde321c92342a2e42427542cdbe
MrCrambo <kadirov.il.crambo@gmail.com>
2020-01-09 23:33:40 +0300

    correlation chart added

--- Finance Predictions/sp_manipul.py
+++ Finance Predictions/sp_manipul.py
@@ -1,9 +1,16 @@
 import bs4 as bs
 import requests
 import os
+import numpy as np
 import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+style.use("ggplot")
 
 ###########################################
 ##                                       ##
@@ -69,4 +76,37 @@ def combine_data():
     print(main_df.head())
     main_df.to_csv('sp500_combined_adj_close.csv')
 
-combine_data()
+###########################################
+##                                       ##
+##    Correlation chart for all sp500    ##
+##                                       ##
+###########################################
+def visualise_data():
+    df = pd.read_csv('sp500_combined_adj_close.csv')
+    df_corr = df.corr()
+    print(df_corr.head())
+
+
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap.set_clim(-1, 1)
+
+    plt.tight_layout()
+    plt.show()
+
+visualise_data()
